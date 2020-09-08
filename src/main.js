e08747b3ab2251c04ff3d426d57e76e2fe934b4a
Darin <darindev1000@gmail.com>
2020-09-08 14:03:04 -0700

    Create login view also copy in scss files

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { auth } from './firebase'
+import './assets/scss/app.scss'
 
 Vue.config.productionTip = false
 
@@ -11,6 +12,8 @@ auth.onAuthStateChanged(() => {
   if (!app) {
     app = new Vue({
       router,
+      // provide the store using the "store" option.
+      // this will inject the store instance to all child components.
       store,
       render: h => h(App)
     }).$mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-
+  <div id="login">
+    <section>
+      <div class="col1">
+        <h1>Vuegram</h1>
+        <p>Welcome to the <a href="https://savvyapps.com/" target="_blank">Savvy Apps</a> sample social media web app powered by Vue.js and Firebase.
+          Build this project by checking out The Definitive Guide to Getting Started with Vue.js</p>
+      </div>
+      <div class="col2">
+        <form @submit.prevent>
+          <h1>Welcome Back</h1>
+          <div>
+            <label for="email1">Email</label>
+            <input v-model.trim="loginForm.email" type="text" placeholder="you@email.com" id="email1" />
+          </div>
+          <div>
+            <label for="password1">Password</label>
+            <input v-model.trim="loginForm.password" type="password" placeholder="******" id="password1" />
+          </div>
+          <button @click="login()" class="button">Log In</button>
+          <div class="extras">
+            <a>Forgot Password</a>
+            <a>Create an Account</a>
+          </div>
+        </form>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
+  export default {
+    data() {
+      return {
+        loginForm: {
+          email: '',
+          password: ''
+        }
+      }
+    },
 
-}
+    methods: {
+      login() {
+        this.$store.dispatch('login', {
+          email: this.loginForm.email,
+          password: this.loginForm.password
+        })
+      }
+    }
+  }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
