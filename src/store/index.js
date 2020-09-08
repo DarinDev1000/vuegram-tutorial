a7956a99c8c38483bf7e36f82dbae4ec54cc1021
Darin <darindev1000@gmail.com>
2020-09-08 14:10:50 -0700

    Add Vuex login state and methods with firebase auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as fb from '../firebase'
+import router from '../router/index'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userProfile: {}
   },
   mutations: {
+    setUserProfile(state, val) {
+      state.userProfile = val
+    }
   },
   actions: {
-  },
-  modules: {
+    async login({ dispatch }, form) {
+      // sign user in
+      const { user } = await fb.auth.signInWithEmailAndPassword(form.email, form.password)
+
+      // fetch user profile and set in state
+      dispatch('fetchUserProfile', user)
+    },
+    async fetchUserProfile({ commit }, user) {
+      // fetch user profile
+      const userProfile = await fb.usersCollection.doc(user.uid).get()
+
+      // set user profile in state
+      commit('setUserProfile', userProfile.data())
+      
+      // change route to dashboard
+      router.push('/')
+    },
+    modules: {
+    }
   }
 })
